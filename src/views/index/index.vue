ceea27e65012b075b5bc8d808c0d3ec59ad58535
NK1999 <heweixiang1110@163.com>
2021-07-10 01:15:48 +0800

    提交测试

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,13 @@
+<!--
+ * @ModeRule: 模块作用
+ * @Author: 何维想
+ * @Date: 2021-07-10 01:15:22
+ * @EditAuthor: heweixiang1110@163.com
+ * @LastEditTime: 2021-07-10 01:15:22
+-->
 <template>
   <div>
-    <h2>hello</h2>
+    <h2>hellos</h2>
   </div>
 </template>
 
